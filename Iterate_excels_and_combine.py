91ca58e3a25490fe8395343dc8f4712921665862
ahbon123 <zhaodanghan@gmail.com>
2019-02-03 14:01:04 +0800

    Update Iterate_excels_and_combine.py

--- Iterate_excels_and_combine.py
+++ Iterate_excels_and_combine.py
@@ -31,3 +31,36 @@ df = pd.concat(list(g), ignore_index=True)
 #method 3: list comprehension + concat
 all_data = [pd.read_excel(f) for f in glob.glob("output/test/*.xlsx")]
 df = pd.concat(all_data, ignore_index=True)
+
+#https://stackoverflow.com/questions/20908018/import-multiple-excel-files-into-python-pandas-and-concatenate-them-into-one-dat
+
+#method 1
+import numpy as np
+import pandas as pd
+import glob
+all_data = pd.DataFrame()
+for f in glob.glob("*.xlsx"):
+    df = pd.read_excel(f)
+    all_data = all_data.append(df,ignore_index=True)
+
+# now save the data frame
+writer = pd.ExcelWriter('output.xlsx')
+all_data.to_excel(writer,'sheet1')
+writer.save()
+
+#method 2
+import os
+import pandas as pd
+
+path = os.getcwd()
+files = os.listdir(path)
+files
+files_xls = [f for f in files if f[-3:] == 'xls']
+files_xls
+
+df = pd.DataFrame()
+
+for f in files_xls:
+    data = pd.read_excel(f, 'Sheet1')
+    df = df.append(data)
+df   
