2a0bb8fac42145beec54e1533c4e6a092c36488c
ahbon123 <zhaodanghan@gmail.com>
2019-02-03 10:31:32 +0800

    Create Read_multisheets.py

---
+++ Read_multisheets.py
@@ -0,0 +1,29 @@
+#https://stackoverflow.com/questions/26521266/using-pandas-to-pd-read-excel-for-multiple-worksheets-of-the-same-workbook
+
+import pandas as pd
+
+df = pd.read_excel('excel_file_path.xls')
+# this will read the first sheet into df
+
+xls = pd.ExcelFile('excel_file_path.xls')
+
+# Now you can list all sheets in the file
+xls.sheet_names
+# ['house', 'house_extra', ...]
+
+# to read just one sheet to dataframe:
+df = pd.read_excel(file_name, sheetname="house")
+
+# to read all sheets to a map
+sheet_to_df_map = {}
+for sheet_name in xls.sheet_names:
+    sheet_to_df_map[sheet_name] = xls.parse(sheet_name)
+    
+# @ihightower pointed out in the comments that all sheets can be 
+# directly read into an ordered dictionary in 1 step
+
+# for pandas version >= 0.21.0
+sheet_to_df_map = pd.read_excel(file_name, sheet_name=None)
+
+# for pandas version < 0.21.0
+sheet_to_df_map = pd.read_excel(file_name, sheetname=None)
