7e09e81fd87ac0f0a60c0d7ed8660465592096ff
ahbon123 <zhaodanghan@gmail.com>
2019-02-03 13:55:43 +0800

    Update Iterate_excels_and_combine.py

--- Iterate_excels_and_combine.py
+++ Iterate_excels_and_combine.py
@@ -12,3 +12,22 @@ for f in glob.glob('myfolder/mydata*.xlsx'):
 writer = pd.ExcelWriter('mycollected_data.xlsx', engine='xlsxwriter')
 all_data.to_excel(writer, sheet_name='Sheet1')
 writer.save()
+
+
+#https://stackoverflow.com/questions/46930575/append-multiple-excel-filesxlsx-together-in-python
+
+#method 1: list and pd.concat
+all_data = []
+for f in glob.glob("output/test/*.xlsx"):
+    all_data.append(pd.read_excel(f))
+      
+df = pd.concat(all_data, ignore_index=True)
+
+#method 2: map and for loop
+
+g = map(pd.read_excel, glob.glob("output/test/*.xlsx"))
+df = pd.concat(list(g), ignore_index=True)
+
+#method 3: list comprehension + concat
+all_data = [pd.read_excel(f) for f in glob.glob("output/test/*.xlsx")]
+df = pd.concat(all_data, ignore_index=True)
